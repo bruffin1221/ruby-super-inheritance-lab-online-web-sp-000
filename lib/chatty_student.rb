cae2506cb8bd5975ce9ed43b53fa20d5e4a30f89
Bruce Ruffin <bruffin1221@gmail.com>
2019-12-07 00:08:45 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,10 +2,17 @@ class ChattyStudent<Student
 
 def hello
   super
-  puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch?
-   The Walking Dead last night? You didn't?!
-   Oh man, it was so crazy! What, you don't want any spoilers?
-   Okay well let me just tell you who died..."
+  puts "How are you doing today?"
+  super
+  puts "I'm okay, but I'm kind of tired. Did you watch?"
+  super
+  puts "The Walking Dead last night? You didn't?!"
+  super
+  puts  "Oh man, it was so crazy! What, you don't want any spoilers?"
+  super
+  puts  "Okay well let me just tell you who died..."
 end
 
-end 
+
+
+end
